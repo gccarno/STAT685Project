6afe134bd2021fa8da0eb275a9934fc631413ffd
gccarno <gccarnovale@gmail.com>
2019-08-30 13:29:27 -0400

    Update mapping.R

--- mapping.R
+++ mapping.R
@@ -8,8 +8,6 @@ aoi_boundary_texas <- st_read(
   "E:/STAT685/Data/shapefile/Current_Districts.shp")
 
 ggplot() + 
-  geom_sf(data = aoi_boundary_texas, size = 3, color = "black", fill = "cyan1") + 
-  ggtitle("AOI Boundary Plot") + 
   geom_sf(data = aoi_boundary_texas,size=1, color = "black", fill = "cyan1") + 
   ggtitle("Texas Schools") + 
   coord_sf()
